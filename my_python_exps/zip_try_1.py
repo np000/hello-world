e14a46a286893960f1a2b646e2676cc1575153c9
npv0 <yureka.npv@gmail.com>
2017-02-28 20:15:25 +0530

    Folder for Experiment scripts in Python
    This folder isfor Experiment scripts in Python

---
+++ my_python_exps/zip_try_1.py
@@ -0,0 +1,23 @@
+﻿
+'''
+import os
+import zipfile
+
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            os.path.relpath(os.‌​path.join(root, file), os.path.join(path, '..'))
+            ziph.write(os.path.join(root, file))
+            
+
+if __name__ == '__main__':
+    zipf = zipfile.ZipFile('Python.zip', 'w', zipfile.ZIP_DEFLATED)
+    zipdir('C:\Users\uidj7182\Documents\python_files\zip', zipf)
+    zipf.close()
+'''
+
+import shutil
+dir_name = 'C:\Users\uidj7182\Documents\python_files\zip'
+output_filename = 'temp'
+shutil.make_archive(output_filename, 'zip', dir_name)
