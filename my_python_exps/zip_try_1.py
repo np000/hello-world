82c485bac2638e65bb613ae9ee02d4cb3b9d2763
npv0 <yureka.npv@gmail.com>
2017-05-18 20:40:38 +0530

    Delete zip_try_1.py

--- my_python_exps/zip_try_1.py
+++
@@ -1,9 +0,0 @@
-﻿
-
-
-import shutil
-dir_name = 'C:\Users\uidj7182\Documents\python_files\zip'
-output_filename = 'temp'
-shutil.make_archive(output_filename, 'zip', dir_name)
-
-# Script to zip zip a folder
